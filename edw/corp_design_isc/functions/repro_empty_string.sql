9a9812139ff158186285f2babb05aeb2b0f3f3fb
chandru-8388 <nallasivammunnur360@gmail.com>
2026-02-04 10:23:59 +0530

    Update repro_empty_string.sql

--- edw/corp_design_isc/functions/repro_empty_string.sql
+++ edw/corp_design_isc/functions/repro_empty_string.sql
@@ -9,51 +9,54 @@ from datetime import datetime
 
 class ShiftScheduler:
 
-    def __init__(self):
-        self.shift_meta_dict = {}
+    def __init__(self):
+        self.shift_meta_dict = {}
 
-    def _intersect_windows(self, a_list, b_list):
-        out = []
-        a = sorted(a_list, key=lambda x: x[0])
-        b = sorted(b_list, key=lambda x: x[0])
-        ia = 0
-        ib = 0
+    def _intersect_windows(self, a_list, b_list):
+        out = []
+        a = sorted(a_list, key=lambda x: x[0])
+        b = sorted(b_list, key=lambda x: x[0])
+        ia = 0
+        ib = 0
 
-        while ia < len(a) and ib < len(b):
-            s = max(a[ia][0], b[ib][0])
-            e = min(a[ia][1], b[ib][1])
-            if e > s:
-                out.append((s, e))
+        while ia < len(a) and ib < len(b):
+            s = max(a[ia][0], b[ib][0])
+            e = min(a[ia][1], b[ib][1])
+            if e > s:
+                out.append((s, e))
 
-            if a[ia][1] < b[ib][1]:
-                ia += 1
-            else:
-                ib += 1
+            if a[ia][1] < b[ib][1]:
+                ia += 1
+            else:
+                ib += 1
 
-        return out
+        return out
 
-    def _find_shift_and_break(self, dt):
-        day_obj = dt
+    def _find_shift_and_break(self, dt):
+        day_obj = dt
 
-        # CUSTOMER CASE: empty string returns
-        if day_obj not in self.shift_meta_dict:
-            return "", ""
+        # CUSTOMER CASE: empty string returns
+        if day_obj not in self.shift_meta_dict:
+            return "", ""
 
-        for meta in self.shift_meta_dict.get(day_obj, []):
-            interval = meta.get("interval")
-            if interval:
-                return meta.get("shift_name", ""), meta.get("break_name", "")
+        for meta in self.shift_meta_dict.get(day_obj, []):
+            interval = meta.get("interval")
+            if interval:
+                return meta.get("shift_name", ""), meta.get("break_name", "")
 
-        return "", ""
+        return "", ""
 
 
 
 def run(dt):
-    scheduler = ShiftScheduler()
-    shift_name, break_name = scheduler._find_shift_and_break(dt)
+    scheduler = ShiftScheduler()
+    shift_name, break_name = scheduler._find_shift_and_break(dt)
 
-    if shift_name == "" and break_name == "":
-        return "", ""
+    if shift_name == "" and break_name == "":
+        return "", ""
 
-    return shift_name, break_name
+    return shift_name, break_name
 $$;
+$
+$
+
